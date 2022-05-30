0a62f0170d0df95b2e1970957e10133def406f4a
Arnav Mundkur <mundkurarnav@gmail.com>
2022-05-30 17:26:32 +0200

    Fixed unique child warning.

--- src/Screens/Profile/Profile.tsx
+++ src/Screens/Profile/Profile.tsx
@@ -201,7 +201,7 @@ export const Profile = () => {
           }}
         >
           {selectedTemperaments.map((t) => {
-            return <TemperamentPill temperament={t} />
+            return <TemperamentPill temperament={t} key={t} />
           })}
         </View>
       </View>
